5ec3230f480469c73bf4214202a1cc612953e6e0
zapataalva <zapataalva4@gmail.com>
2026-03-19 12:20:47 -0500

    Se sube la configuracion para usar las variables del app env

---
+++ src/core/config.py
@@ -0,0 +1,24 @@
+﻿from pydantic import BaseModel
+from functools import lru_cache
+import os
+from dotenv import load_dotenv
+
+load_dotenv("app.env")
+
+class Settings(BaseModel):
+    request_timeout_seconds: float = 15.0
+    connect_timeout_seconds: float = 7.0
+    total_scan_timeout_seconds: float = 40.0
+    user_agent: str = "SecurityScan"
+
+    mongo_uri: str
+    mongo_db_name: str
+    mongo_collection_name: str
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings(
+        mongo_uri=os.getenv("MONGO_URI"),
+        mongo_db_name=os.getenv("MONGO_DB"),
+        mongo_collection_name=os.getenv("MONGO_COLLECTION"),
+    )
---
+++ src/core/dependencies.py
@@ -0,0 +1,6 @@
+﻿from src.core.config import get_settings
+from src.infrastructure.repositories.mongo_scan_repository import MongoScanRepository
+
+def get_scan_repository():
+    settings = get_settings()
+    return MongoScanRepository(settings)
